02356a135e0b9ccf31e9e16763b94b588b89b8d0
Gavin Liburd <GavinLiburd901@gmail.com>
2022-05-26 05:19:41 -0400

    Add RockPaperScissors function

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 function computerPlay() {
-  const choices = ["Rock", "Paper", "Scissors"]
+  const choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
 }
 console.log(computerPlay)
+
+function RockPaperScissors(playerSelection, computerSelection=computerPlay()) {
+  playerSelection = playerSelection.toLowerCase();
+  if (playerSelection === "rock" && computerSelection === "scissors") {
+    return("You win! Rock beats Scissors")
+  } else if (playerSelection === "rock" && computerSelection == "paper") {
+    return("You lose! Paper beats Rock")
+  } else if (playerSelection === "rock" && computerSelection === "rock") {
+    return("You neither win nor lose! Rock can't beat Rock")
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return("You win! Paper beats Rock")
+  } else if (playerSelection === "paper" && computerSelection === "paper") {
+    return("You neither win nor lose! Paper can't beat Paper")
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return("You lose! Scissors beats Paper")
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return("You lose! Rock beats Scissors")
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return("You win! Scissors beats Paper")
+  } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+    return("You neither win nor lose! Scissors can't beat Scissors")
+  }
+}
