271618f0a5114ed878a6250ded500c50c2fbdf9b
Gavin Liburd <GavinLiburd901@gmail.com>
2022-05-26 20:45:53 -0400

    Create game function

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ function computerPlay() {
   const choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
 }
-console.log(computerPlay)
 
-function RockPaperScissors(playerSelection, computerSelection=computerPlay()) {
-  playerSelection = playerSelection.toLowerCase();
+
+
+function playRound(playerSelection, computerSelection=computerPlay()) {
+  playerSelection = prompt("Choose between Rock, Paper, or Scissors").toLowerCase()
   if (playerSelection === "rock" && computerSelection === "scissors") {
     return("You win! Rock beats Scissors")
   } else if (playerSelection === "rock" && computerSelection == "paper") {
@@ -24,5 +25,29 @@ function RockPaperScissors(playerSelection, computerSelection=computerPlay()) {
     return("You win! Scissors beats Paper")
   } else if (playerSelection === "scissors" && computerSelection === "scissors") {
     return("You neither win nor lose! Scissors can't beat Scissors")
+  } 
+}
+
+let userScore = 0
+let computerScore = 0
+
+for (let i = 0; i < 5; i++) {
+  let roundResult = playRound()
+  if (roundResult.includes("You win")) {
+    userScore += 1
+  } else if (roundResult.includes("You lose")) {
+    computerScore += 1
+}
+  if (i < 4) {
+  console.log(roundResult)
+  } else if (i = 4) {
+    if (userScore > computerScore) {
+      console.log(`You won the game! You have ${userScore} wins and the computer has ${computerScore} wins`)
+    } else if (userScore < computerScore) {
+      console.log(`You lost the game! You have ${userScore} wins and the computer has ${computerScore} wins`)
+    } else {
+      console.log(`The game ends in a tie! You have ${userScore} wins and the computer has ${computerScore} wins`)
+    }
   }
 }
+
